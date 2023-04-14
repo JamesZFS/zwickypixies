3d5f35fbc0ea3a2b87850481a4824430609d859d
Kyrus1999 <71381995+Kyrus1999@users.noreply.github.com>
2023-04-14 21:45:59 +0200

    File reader

--- animation.py
+++ animation.py
@@ -2,8 +2,11 @@
 
 import vtkmodules.vtkInteractionStyle
 import vtkmodules.vtkRenderingOpenGL2
+from vtkmodules.vtkCommonColor import vtkNamedColors
 from vtkmodules.vtkCommonDataModel import vtkPolyData
 from vtkmodules.vtkFiltersSources import vtkSphereSource
+from vtkmodules.vtkIOXML import vtkXMLPolyDataReader
+import re
 from vtkmodules.vtkRenderingCore import (
     vtkActor,
     vtkPolyDataMapper,
@@ -12,6 +15,10 @@ from vtkmodules.vtkRenderingCore import (
     vtkRenderWindowInteractor,
     vtkRenderer
 )
+
+from helpers import print_meta_data, slice_polydata
+
+
 
 
 def trace_particle(data_dir: str, particle_ids: list):
@@ -23,4 +30,17 @@ def trace_particle(data_dir: str, particle_ids: list):
     '''
     # I'm not sure how to do this yet, but I think vtk has some animation support
     # https://kitware.github.io/vtk-examples/site/Python/Utilities/Animation/
-    raise NotImplementedError
+    reader = vtkXMLPolyDataReader()
+    colors = vtkNamedColors()
+
+    # split path arround file number
+    name = re.split(r'\d\d\d', data_dir)
+    # iterate over all files
+
+    final = vtkPolyData()
+    for num in range(0, 625, 2):
+        if num > 10: break;
+        fileName = name[0] + "{0:03d}".format(num) + name[1]
+        print(fileName)
+
+
